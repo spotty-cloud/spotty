e7550e073786dadd035099c0599b11ff5dbadb3a
Oleg Polosin <apls777@gmail.com>
2018-07-31 00:11:56 +0100

    logs for user scripts

--- spotty/commands/run.py
+++ spotty/commands/run.py
@@ -31,6 +31,10 @@ class RunCommand(AbstractConfigCommand):
         project_name = project_config['name']
         region = instance_config['region']
 
+        script_name = self._args.script_name
+        if script_name not in self._config['scripts']:
+            raise ValueError('Script "%s" is not defined in the configuration file.' % script_name)
+
         cf = boto3.client('cloudformation', region_name=region)
         stack = StackResource(cf, project_name, region)
 
@@ -45,8 +49,7 @@ class RunCommand(AbstractConfigCommand):
         host = 'ubuntu@%s' % ip_address
         key_path = KeyPairResource(None, project_name, region).key_path
 
-        # run a new command or attach to already running one
-        script_name = self._args.script_name
+        # run a script or attach to already running one
         session_name = self._args.session_name if self._args.session_name else 'spotty-script-%s' % script_name
         script_base64 = base64.b64encode(self._config['scripts'][script_name].encode('utf-8')).decode('utf-8')
         script_path = '/tmp/docker/%s.sh' % script_name
@@ -54,9 +57,11 @@ class RunCommand(AbstractConfigCommand):
 
         attach_session_cmd = subprocess.list2cmdline(['tmux', 'attach', '-t', session_name])
         upload_script_cmd = subprocess.list2cmdline(['echo', script_base64, '|', 'base64', '-d', '>', script_path])
-        new_session_cmd = subprocess.list2cmdline(['tmux', 'new', '-s', session_name, '-A',
-                                                   'sudo', 'docker', 'exec', '-it', '-w', working_dir, 'spotty',
-                                                   '/bin/bash', '-xe', script_path])
+        docker_cmd = subprocess.list2cmdline(['sudo', 'docker', 'exec', '-it', '-w', working_dir, 'spotty',
+                                              '/bin/bash', '-xe', script_path, '2>&1', '|', 'sudo', 'tee', '-a',
+                                              '/var/log/spotty-run/%s.log' % session_name])
+        new_session_cmd = subprocess.list2cmdline(['tmux', 'new', '-s', session_name, docker_cmd])
+
 
         remote_cmd = '%s || (%s && %s)' % (attach_session_cmd, upload_script_cmd, new_session_cmd)
         ssh_command = ['ssh', '-i', key_path, '-o', 'StrictHostKeyChecking no', host, '-t', remote_cmd]
--- spotty/commands/start.py
+++ spotty/commands/start.py
@@ -77,16 +77,19 @@ class StartCommand(AbstractConfigCommand):
                                                output=output)
 
         if status == 'CREATE_COMPLETE':
+            ip_address = [row['OutputValue'] for row in info['Outputs'] if row['OutputKey'] == 'InstanceIpAddress'][0]
             log_group = [row['OutputValue'] for row in info['Outputs'] if row['OutputKey'] == 'InstanceLogGroup'][0]
 
             output.write('\n'
                          '--------------------\n'
                          'Instance is running.\n'
+                         '\n'
+                         'IP address: %s\n'
                          'CloudWatch Log Group:\n'
                          '  %s\n'
                          '\n'
-                         'Use "spotty ssh" command to connect to the instance.\n'
-                         '--------------------' % log_group)
+                         'Use "spotty ssh" command to connect to the Docker container.\n'
+                         '--------------------' % (ip_address, log_group))
         else:
             raise ValueError('Stack "%s" was not created.\n'
                              'Please, see CloudFormation and CloudWatch logs for the details.' % stack.name)
