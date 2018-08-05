a9c5de54b8426f91c1352db15f78df7f0998cabe
Oleg Polosin <apls777@gmail.com>
2018-08-05 02:50:25 +0100

    update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='spotty',
-      version='1.0.1',
+      version='1.0.2',
       description='Train deep learning models on AWS EC2 Spot Instances',
       url='http://github.com/apls777/spotty',
       author='Oleg Polosin',
