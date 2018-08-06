43a2e38e751acbe71a4249f58fb34423a1120567
Oleg Polosin <apls777@gmail.com>
2018-08-06 15:21:25 +0100

    clean-logs command, disable AMI awslogs service

---
+++ spotty/commands/clean_logs.py
@@ -0,0 +1,60 @@
+from argparse import ArgumentParser
+import boto3
+from spotty.commands.abstract_config import AbstractConfigCommand
+from spotty.commands.helpers.validation import validate_logs_config
+from spotty.commands.writers.abstract_output_writrer import AbstractOutputWriter
+
+
+class CleanLogsCommand(AbstractConfigCommand):
+
+    @staticmethod
+    def get_name() -> str:
+        return 'clean-logs'
+
+    @staticmethod
+    def get_description():
+        return 'Delete CloudFormation log groups with Spotty prefixes'
+
+    @staticmethod
+    def _validate_config(config):
+        return validate_logs_config(config)
+
+    @staticmethod
+    def configure(parser: ArgumentParser):
+        AbstractConfigCommand.configure(parser)
+        parser.add_argument('--delete-all', '-a', action='store_true', help='Delete all Spotty log groups, '
+                                                                            'not just empty ones')
+
+    def run(self, output: AbstractOutputWriter):
+        region = self._config['instance']['region']
+        logs = boto3.client('logs', region_name=region)
+
+        prefixes = ['spotty-', '/aws/lambda/spotty-']
+        only_empty = not self._args.delete_all
+
+        output.write('Deleting %s Spotty log groups...' % ('empty' if only_empty else 'all'))
+
+        res = logs.describe_log_groups()
+        self._delete_log_groups(logs, res['logGroups'], prefixes, only_empty, output)
+
+        while 'nextToken' in res:
+            res = logs.describe_log_groups(nextToken=res['nextToken'])
+            self._delete_log_groups(logs, res['logGroups'], prefixes, only_empty, output)
+
+        output.write('Done')
+
+    @staticmethod
+    def _delete_log_groups(logs, log_groups: list, prefixes: list, only_empty: bool, output: AbstractOutputWriter):
+        for log_group in log_groups:
+            for prefix in prefixes:
+                if log_group['logGroupName'].startswith(prefix):
+                    delete = True
+                    if only_empty:
+                        delete = False
+                        res = logs.describe_log_streams(logGroupName=log_group['logGroupName'], limit=1)
+                        if not len(res['logStreams']):
+                            delete = True
+                    if delete:
+                        output.write('[x] %s' % log_group['logGroupName'])
+                        logs.delete_log_group(logGroupName=log_group['logGroupName'])
+                    break
