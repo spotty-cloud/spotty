f4146a503f784cccdde54583c8832e7e2160e08d
Oleg Polosin <apls777@gmail.com>
2018-09-08 00:04:56 +0100

    updated CF template for AMI creation, fixes

--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
 #!/usr/bin/env python
 
+import os
+import re
 from setuptools import setup, find_packages
 
 
+def get_version():
+    root_dir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(root_dir, 'spotty', '__init__.py')) as f:
+        content = f.read()
+
+    version_match = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', content, re.M)
+    if not version_match:
+        raise RuntimeError('Unable to find version string.')
+
+    return version_match.group(1)
+
+
 setup(name='spotty',
-      version='1.0.5',
+      version=get_version(),
       description='Train deep learning models on AWS EC2 Spot Instances',
       url='http://github.com/apls777/spotty',
       author='Oleg Polosin',
       author_email='apls777@gmail.com',
       license='MIT',
       packages=find_packages(exclude=['tests*']),
-      package_data={'spotty': ['data/create_ami.yaml', 'data/run_container.yaml']},
+      package_data={'spotty': [
+          'data/create_ami.yaml',
+          'data/run_container.yaml',
+          'data/create_instance_profile.yaml',
+      ]},
       scripts=['bin/spotty'],
       install_requires=[
           'botocore>=1.10.0',
