9c10188ee45b86c891a08a7560a8ff9049bf6b6c
Matthew Beckers <17108752+mattlbeck@users.noreply.github.com>
2021-03-29 23:09:31 +0100

    validate inboundIp param using netaddr
    netaddr.IPNetwork used as part of the schema validation and type for
    inbound_ip param.

--- spotty/providers/aws/cfn_templates/instance/template.py
+++ spotty/providers/aws/cfn_templates/instance/template.py
@@ -20,7 +20,6 @@ from spotty.providers.aws.resources.volume import Volume
 from spotty.providers.aws.config.instance_config import InstanceConfig
 from spotty.providers.aws.config.ebs_volume import EbsVolume
 from spotty.providers.aws.helpers.logs import get_logs_s3_path
-from netaddr import IPNetwork
 
 
 def prepare_instance_template(ec2, instance_config: InstanceConfig, docker_commands: DockerCommands,
@@ -58,11 +57,11 @@ def prepare_instance_template(ec2, instance_config: InstanceConfig, docker_comma
         del template['Resources']['InstanceLaunchTemplate']['Properties']['LaunchTemplateData']['SecurityGroupIds']
 
     # add ports to the security group for the user-defined inbound IP
-    ipnetwork = IPNetwork(instance_config.instance_inbound_ip)
-    cidr_ip = 'CidrIpv6' if ipnetwork.version == 6 else 'CidrIp'
+    inbound_ip = instance_config.inbound_ip
+    cidr_ip = 'CidrIpv6' if inbound_ip == 6 else 'CidrIp'
     for port in [22] + instance_config.ports:
         template['Resources']['InstanceSecurityGroup']['Properties']['SecurityGroupIngress'] += [{
-            cidr_ip: str(ipnetwork),
+            cidr_ip: str(inbound_ip),
             'IpProtocol': 'tcp',
             'FromPort': port,
             'ToPort': port,
