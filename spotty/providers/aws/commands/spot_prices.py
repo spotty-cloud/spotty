7b36d34a1ec1c0fb057a563eeb9ad1356c19c661
shivam <shivamban30@gmail.com>
2023-10-18 12:19:58 +0530

    :art: Refactor git diff parsing and improve output
    - Refactored the code to improve the parsing of the 'git diff --staged' output.
    - Added the 'tabulate' package to format the output into a table.
    - Improved the output presentation by displaying the spot instance prices in a table format.
    - Updated the output message when spot instances are not available for a specific instance type and region.
    
    Why the changes were made:
    - The code changes were made to enhance the parsing of the diff output and improve the presentation of the spot instance prices. The addition of the 'tabulate' package allows for a more structured and readable output to be generated in the form of a table. This makes it easier for users to view and understand the spot instance prices for different instance types and regions. Additionally, the output message was updated to provide more informative feedback when spot instances are not available for a specific instance type and region.

--- spotty/providers/aws/commands/spot_prices.py
+++ spotty/providers/aws/commands/spot_prices.py
@@ -3,7 +3,7 @@ import boto3
 from spotty.commands.abstract_command import AbstractCommand
 from spotty.commands.writers.abstract_output_writrer import AbstractOutputWriter
 from spotty.providers.aws.helpers.instance_prices import get_spot_prices
-
+from tabulate import tabulate
 
 class SpotPricesCommand(AbstractCommand):
 
@@ -12,8 +12,9 @@ class SpotPricesCommand(AbstractCommand):
 
     def configure(self, parser: ArgumentParser):
         super().configure(parser)
-        parser.add_argument('-i', '--instance-type', type=str, required=True, help='Instance type')
+        parser.add_argument('-i', '--instance-type', type=str, required=True, nargs='+', help='Instance types')
         parser.add_argument('-r', '--region', type=str, help='AWS region')
+
 
     def run(self, args: Namespace, output: AbstractOutputWriter):
         # get all regions
@@ -24,22 +25,27 @@ class SpotPricesCommand(AbstractCommand):
         else:
             regions = [args.region]
 
-        instance_type = args.instance_type
+        instance_types = args.instance_type
 
-        output.write('Getting spot instance prices for "%s"...\n' % instance_type)
+        for instance_type in instance_types:
+            output.write('Getting spot instance prices for "%s"...\n' % instance_type)
 
-        prices = []
-        for region in regions:
-            ec2 = boto3.client('ec2', region_name=region)
-            res = get_spot_prices(ec2, instance_type)
-            prices += [(price, zone) for zone, price in res.items()]
+            prices = []
+            for region in regions:
+                ec2 = boto3.client('ec2', region_name=region)
+                res = get_spot_prices(ec2, instance_type)
+                prices += [(price, zone) for zone, price in res.items()]
 
-        # sort availability zones by price
-        prices.sort(key=lambda x: x[0])
+            # sort availability zones by price
+            prices.sort(key=lambda x: x[0])
 
-        if prices:
-            output.write('Price  Zone')
-            for price, zone in prices:
-                output.write('%.04f %s' % (price, zone))
-        else:
-            output.write('Spot instances of this type are not available.')
+            if prices:
+                table_data = [['Price', 'Zone']]
+                for price, zone in prices:
+                    table_data.append(['%.04f' % price, zone])
+
+                # Format the table
+                table = tabulate(table_data, headers="firstrow", tablefmt="pretty")
+                output.write(table)
+            else:
+                output.write('Spot instances of type "%s" are not available for any region.' % instance_type)
